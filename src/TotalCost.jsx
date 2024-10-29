9d27cd514a84da78fc9414cc8d4d3f7571a25f23
Jhonattan Orozco <Jhonattan.Orozco@theksquaregroup.com>
2024-10-28 19:47:13 -0600

    exercise complete

--- src/TotalCost.jsx
+++ src/TotalCost.jsx
@@ -1,27 +1,35 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import "./TotalCost.css";
 
 const TotalCost = ({ totalCosts, ItemsDisplay }) => {
-  
+  const { venue, addOn, meals } = totalCosts;
+  const total_amount = venue + addOn + meals;
 
   return (
     <div className="pricing-app">
       <div className="display_box">
         <div className="header">
-          <p className="preheading"><h3>Total cost for the event</h3></p>
+          <p className="preheading">
+            <h3>Total cost for the event</h3>
+          </p>
         </div>
         <div>
           <h2 id="pre_fee_cost_display" className="price">
-           
+            ${total_amount}
           </h2>
-         
-            <div>
-             
-            </div>
+          <div className="render_items">
+            <ItemsDisplay />
+          </div>
         </div>
       </div>
     </div>
   );
+};
+
+TotalCost.propTypes = {
+  totalCosts: PropTypes.object,
+  ItemsDisplay: PropTypes.elementType,
 };
 
 export default TotalCost;
--- src/store.js
+++ src/store.js
@@ -1,9 +1,13 @@
 // store.js
-import { configureStore } from '@reduxjs/toolkit';
-import venueReducer from './venueSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import venueReducer from "./venueSlice";
+import addOnReducer from "./addOnSlice";
+import mealsReducer from "./mealsSlice";
 
 export default configureStore({
   reducer: {
     venue: venueReducer,
+    addOn: addOnReducer,
+    meals: mealsReducer,
   },
 });
